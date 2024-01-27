3a45e3a23ae710d26296a10fb5dc74408a813ea6
m2zhang <marilyn2zhang@gmail.com>
2024-01-27 15:43:34 -0500

    Updated food_api and got cohere
    may or may not need to remove value in landing page.
    
    I put the food_api in the form of a function! Call on it and pass through an image's path

--- food_api.py
+++ food_api.py
@@ -1,17 +1,24 @@
 import requests
 
-img = '/Users/marilynzhang/Desktop/Banana-4183238557.jpg' 
-api_user_token = 'f08be828bbf2a3a4c1185de9ff1cc307b15875be'
-headers = {'Authorization': 'Bearer ' + api_user_token}
+#saves a list of the different items!
+# img format should be the file path of the image. Ex: /User
+def get_ingredients(img):
+    api_user_token = 'fb5a4c7df78e749b63d71a34cfd375c689a8e441'
+    headers = {'Authorization': 'Bearer ' + api_user_token}
 
-# Single/Several Dishes Detection
-url = 'https://api.logmeal.es/v2/image/segmentation/complete'
-resp = requests.post(url,files={'image': open(img, 'rb')},headers=headers)
+    # Single/Several Dishes Detection
+    url = 'https://api.logmeal.es/v2/image/segmentation/complete'
+    resp = requests.post(url,files={'image': open(img, 'rb')},headers=headers)
 
-# Ingredients information
-url = 'https://api.logmeal.es/v2/recipe/ingredients'
-response = requests.post(url,json={'imageId': resp.json()['imageId']}, headers=headers)
-final_info = response.json() # display ingredients info
+    # Ingredients information
+    url = 'https://api.logmeal.es/v2/recipe/ingredients'
+    response = requests.post(url,json={'imageId': resp.json()['imageId']}, headers=headers)
+    final_info = response.json() # display ingredients info
 
-food_name = final_info['foodName'][0]
-print(food_name)
+    food_name = final_info['foodName']
+    return food_name
+
+
+# Test of the call
+#food_name_test = get_ingredients('/Users/marilynzhang/Desktop/food_test_2.jpg')
+#print(food_name_test)
