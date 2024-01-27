893f19cc771f7b55add934def85cc8e683dd4a5b
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-01-27 07:49:23 -0500

    Changed the background to lightmode, and slight adjustments to the navbar

--- src/confirm_ing/confirm_ing.py
+++ src/confirm_ing/confirm_ing.py
@@ -1,22 +1,32 @@
 from taipy import *
 
-
 path = ""
-content = "src/img/MBTI.png" #user uploaded image here
+content = "src/img/MBTI.png" # insert user uploaded image here
 
 page_file = """
-<|{content}|image|>
+
+<|{content}|image|width="400px"|height="400px"|>
+
 
 
 """
-# [<img=content >](content)
+# ![picture](/img/MBTI.png)
+# <|{content}|image|>
+# <img src=content alt="image" width="50%" height="auto">
 # <|{path}|file_selector|extensions=.jpg|label=Upload .txt file|on_action=analyze_file|> <|{f'Downloading {treatment}%...'}|>
 
-pages = {"/":"<center>\n<|navbar|>\n</center>",
-         "home":'',
-         "ingredients":page_file}
 
+pages = {"/":"<|navbar|id=nav|>",
+         "home":'',
+         "ingredients":page_file,
+         "recipes":''}
+
+
+stylekit = {
+  "color_primary": "#000000",
+  "color_secondary": "#FDA2A2",
+}
 
 # <|toggle|theme|>
 
-Gui(pages=pages).run()
+Gui(pages=pages,css_file="src/confirm_ing/confirm_ing.css").run(dark_mode=False, stylekit=stylekit)
