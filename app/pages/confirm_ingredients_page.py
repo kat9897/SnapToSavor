52bf4eaf25a1ef36da55f9dd709679e86f67370a
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-01-27 11:44:39 -0500

    updated missing_img and confirm page

--- app/pages/confirm_ingredients_page.py
+++ app/pages/confirm_ingredients_page.py
@@ -1,30 +1,34 @@
 from taipy.gui import navigate
-from PIL import Image
 
 # Bindings
-image = Image.open("app/pages/img/missing_img.jpg") # default image without any user input
-resized_image = image.resize(400,400)
-resized_image.save("app/pages/img/missing_img.jpg")
+image = "app/pages/img/missing_img.jpg" # default image without any user input
 
 num_ingred = 0
 
 confirm_ingredients_page="""
-<|layout|columns=2 2|
+<|layout|columns=2 3|
 <|card card-bg|
-<|{resized_image}|image|label="Uploaded Fridge Image"|>
+<|{image}|image|> 
 |>
 <|
 <|
 <|card card-bg|
 <|Confirm {num_ingred} ingredient(s):|>\n
 |>
-<|{selected_files}|file_selector|label=Upload File|on_action=confirmed_ingred|extensions=.jpg,.jpeg,.png|drop_message=Drop Message|multiple|>
 |>
 |>
 <|Confirm|button|on_action=confirmed_ingred|>
 """
 
+# |label="Uploaded Fridge Image"
+
 def confirmed_ingred(state):
     navigate(state, "recipes")
 
+
+def resize_img(img):
+    '''
+    Resize the image img to a desired size.
+    '''
+    pass
 
