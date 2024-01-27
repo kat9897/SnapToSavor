6ea8cb67fb745e811293fe7e583a55a9001ba8d9
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-01-27 10:22:27 -0500

    Changed visual layout of confirm page

--- app/pages/confirm_ingredients_page.py
+++ app/pages/confirm_ingredients_page.py
@@ -1,18 +1,21 @@
 from taipy.gui import navigate
 
 # Bindings
-image = "app/pages/img/MBTI.png"
+image = "app/pages/img/image-missing.svg" # default image without any user input
 num_ingred = 0
 
 confirm_ingredients_page="""
 <|layout|columns=2 2|
-<|
+<|card card-bg|
 <|{image}|image|label="Uploaded Fridge Image"|>
 |>
 <|
-<|Confirm {num_ingred} ingredient(s):|>
+<|
+<|card card-bg|
+<|Confirm {num_ingred} ingredient(s):|>\n
 |>
 <|{selected_files}|file_selector|label=Upload File|on_action=upload_files|extensions=.jpg,.jpeg,.png|drop_message=Drop Message|multiple|>
+|>
 |>
 """
 
