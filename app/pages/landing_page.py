be077968091f80b078ab813a6aa719db8b446586
m2zhang <marilyn2zhang@gmail.com>
2024-01-27 13:44:25 -0500

    Search bar :p
    Going to try adding cohere to it now

--- app/pages/landing_page.py
+++ app/pages/landing_page.py
@@ -3,10 +3,13 @@ from taipy.gui import navigate
 # Bindings
 selected_files = None
 
+value = "Search for recipe..."
+content = 
+
 landing_page="""
 <|{selected_files}|file_selector|label=Upload File|on_action=uploaded_files|extensions=.jpg,.jpeg,.png|drop_message=Drop Message|>
 #Search for recipe:
-<|{value}|input|>
+<|{value}|input|> <|{content}| image |label=Search | on_action=confirmed_ingred>
 """
 
 def uploaded_files(state):
