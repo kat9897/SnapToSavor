b696a20496ad1686d8e7df7df2cb6b04ebb56752
m2zhang <marilyn2zhang@gmail.com>
2024-01-27 13:08:51 -0500

    Update landing_page.py
    Added a search bar :)

--- app/pages/landing_page.py
+++ app/pages/landing_page.py
@@ -5,6 +5,8 @@ selected_files = None
 
 landing_page="""
 <|{selected_files}|file_selector|label=Upload File|on_action=uploaded_files|extensions=.jpg,.jpeg,.png|drop_message=Drop Message|>
+#Search for recipe:
+<|{value}|input|>
 """
 
 def uploaded_files(state):
