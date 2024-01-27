2d34a2a6f8cf6ae981dea567da40aa9785995f1d
Katrina <katrinaemily.best@mail.utoronto.ca>
2024-01-27 08:34:04 -0500

    multi-page setup
    - ready for logic

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from taipy import Gui
+
+from pages.landing_page import *
+from pages.found_recipes_page import *
+from pages.root_page import *
+from pages.confirm_ingredients_page import *
+
+pages = {
+    "/": root_page,
+    "ingredients": landing_page,
+    "confirm": confirm_ingredients_page,
+    "recipes": found_recipes_page
+}
+
+stylekit = {
+  "color_primary": "#BADA55",
+  "color_secondary": "#C0FFE",
+}
+
+if __name__ == "__main__":
+    Gui(pages=pages).run(debug=True, use_reloader=True, port=8080, stylekit=stylekit)
+
+
