552a839e1755066d56e35084c9dccfc865c4e97b
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-01-27 10:25:53 -0500

    Slight function name change

--- app/pages/confirm_ingredients_page.py
+++ app/pages/confirm_ingredients_page.py
@@ -14,12 +14,13 @@ confirm_ingredients_page="""
 <|card card-bg|
 <|Confirm {num_ingred} ingredient(s):|>\n
 |>
-<|{selected_files}|file_selector|label=Upload File|on_action=upload_files|extensions=.jpg,.jpeg,.png|drop_message=Drop Message|multiple|>
+<|{selected_files}|file_selector|label=Upload File|on_action=confirmed_ingred|extensions=.jpg,.jpeg,.png|drop_message=Drop Message|multiple|>
 |>
 |>
+<|Confirm|button|on_action=confirmed_ingred|>
 """
 
-def upload_files(state):
+def confirmed_ingred(state):
     navigate(state, "recipes")
 
 
