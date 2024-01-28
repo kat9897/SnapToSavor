46b1c2fbc79a9fd6bb418a2727bfd29c4cc7d81c
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-01-28 00:11:18 -0500

    image is now able to appear on confirm page

--- app/pages/confirm_ingredients_page.py
+++ app/pages/confirm_ingredients_page.py
@@ -1,15 +1,16 @@
 from taipy.gui import navigate
+from .landing_page import *
 import os
 
 # Bindings
-here = os.path.dirname(os.path.abspath(__file__))
-image = os.path.join(here, "./img/image-missing.svg") # default image without any user input
+# here = os.path.dirname(os.path.abspath(__file__))
+# image = os.path.join(here, "./img/image-missing.svg") # default image without any user input
 num_ingred = 0
 
 confirm_ingredients_page="""
 <|layout|columns=2 3|
 <|card card-bg|
-<|{image}|image|> 
+<|{selected_files}|image|> 
 |>
 <|card card-bg|
 <|Confirm {num_ingred} ingredient(s):|>\n
@@ -21,8 +22,8 @@ confirm_ingredients_page="""
 # |label="Uploaded Fridge Image"
 
 def confirmed_ingred(state):
-    print(image)
-    # navigate(state, "recipes")
+    # print(image)
+    navigate(state, "recipes")
 
 
 def resize_img(img):
