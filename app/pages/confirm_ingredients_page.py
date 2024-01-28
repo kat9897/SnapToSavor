75e7bbbf219f929479aa16f69cc247d6651727a2
Katrina <katrinaemily.best@mail.utoronto.ca>
2024-01-28 05:01:27 -0500

    BIG UPDATE: FILE UPLOAD WORKS
    - LogMeal is weird, thinks butter is cheese
    - onions

--- app/pages/confirm_ingredients_page.py
+++ app/pages/confirm_ingredients_page.py
@@ -2,24 +2,32 @@ from taipy.gui import navigate
 import os
 
 # Bindings
-here = os.path.dirname(os.path.abspath(__file__))
-image = os.path.join(here, "./img/image-missing.svg") # default image without any user input
-num_ingred = 0
+# here = os.path.dirname(os.path.abspath(__file__))
+# image = os.path.join(here, "./img/image-missing.svg") # default image without any user input
+selected_files = None
+ingredients = []
+num_ingred = 1
 
 confirm_ingredients_page="""
 <|layout|columns=2 2|
 <|card card-bg|
-<|{image}|image|label="Uploaded Fridge Image"|>
+<center>
+<|{selected_files}|image|>
+</center>
 |>
 <|card card-bg|
-<|Confirm {num_ingred} ingredient(s):|>\n
+Confirm <|{num_ingred}|text|> ingredient(s):\n
+<|{ingredients[0]}|>
 |>
 <|Confirm|button|on_action=confirmed_ingred|>
 |>
 """
 
+def update_num_ingred(state):
+    num_ingred = num_ingred + 1
+
 def confirmed_ingred(state):
-    print(image)
+    print(state.image)
     # navigate(state, "recipes")
 
 
