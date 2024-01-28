b68f8f09f6634f1947c0a26452b441ebbd1255fc
Guo Cheng <guo32861@gmail.com>
2024-01-28 04:20:19 -0500

    finalsql

--- app/backend/sql_func.py
+++ app/backend/sql_func.py
@@ -4,8 +4,6 @@ from sqlite3 import Error
 import datetime 
 import random
 
-from generate_recipes import *
-
 def sql_connect(database):
     db = None
     cursor = None
@@ -49,12 +47,18 @@ def add_user(db, cursor, FirstName, LastName):
     ##Protien = None, A = None, B = None, C = None, D = None, Fiber = None, Calories = None):
 def add_ingredient(db, cursor, Ingredient):
 
-    insert_query = '''INSERT INTO Ingredients (IngredientID, IngredientName, Description, Sugar_g, Sodium_mg, Fats_g, Protien_g, Vitamin_A_mcg, Vitamin_B_mcg, Vitamin_C_mcg, Vitamin_D_mcg, Fiber_g, Calories)
-    VALUES ("{IngredientID}", "{IngredientName}", "{Description}", "{Sugar_g}", "{Sodium_mg}", "{Fats_g}", "{Protein_g}", "{Vitamin_A_mcg}", "{Vitamin_B_mcg}", "{Vitamin_C_mcg}", "{Vitamin_D_mcg}", "{Fiber_g}", "{Calories}")
-    '''.format(**Ingredient)
-
-    cursor.execute(insert_query)
     db.commit()
+    ing_ids = get_ing_ids(db, cursor)
+    #print(ing_ids)
+    cursor.row_factory = None
+
+    if Ingredient["IngredientID"] not in ing_ids:
+        insert_query = '''INSERT INTO Ingredients (IngredientID, IngredientName)
+        VALUES ("{IngredientID}", "{IngredientName}")
+        '''.format(**Ingredient)
+
+        cursor.execute(insert_query)
+        db.commit()
 
 
 def add_recipe(db, cursor, Recipe):
@@ -79,6 +83,7 @@ def add_recipe(db, cursor, Recipe):
     #Recipe["RecipeThumbnailLink"] = Recipe["RecipeThumbnailLink"].replace('/','?')
     #Recipe["RecipeThumbnailLink"] = Recipe["RecipeThumbnailLink"].replace(':','-')
     Recipe["RecipeThumbnailLink"] = Recipe["RecipeThumbnailLink"].replace(' ','')
+    Recipe["Description"] = ""
 
     insert_recipe = '''INSERT INTO Recipes (RecipeID, RecipeName, UserID, Description, RecipeThumbnailLink, DatePosted)
     VALUES (
@@ -96,36 +101,71 @@ def add_recipe(db, cursor, Recipe):
     for RecipeIngredient in Recipe["RecipeIngredients"]:
         RecipeIngredient["RecipeID"] = RecipeID
 
-        insert_recipe_ingredient = '''INSERT INTO Recipe_Ingredients_STG (RecipeID, IngredientID, Quantity, QuantityUnit)
+        insert_recipe_ingredient = '''INSERT INTO RecipeIngredients_STG (RecipeID, IngredientID, Quantity, QuantityUnit)
         VALUES (
             "{RecipeID}",
-            "{IngredientID}',
-            "{Quantity}",
+            "{IngredientID}",
+            {Quantity},
             "{QuantityUnit}"
         )
         '''.format(**RecipeIngredient)
 
+        #print(insert_recipe_ingredient)
+
         cursor.execute(insert_recipe_ingredient)
+        db.commit()
+
+        add_ingredient(db,cursor,RecipeIngredient)
 
 
-    for Ingredient in Recipe["Ingredients"]:
-        add_ingredient(db, cursor, Ingredient)
 
-    db.commit()
+def get_recipe(db, cursor, RecipeID):
+    select_query = '''SELECT * FROM (
+    SELECT 
+    Recipes.*, 
+    group_concat(Quantity || " " || QuantityUnit || " of " || A.IngredientName, ", ")  AS "IngredientsList"
 
+    FROM Recipes
 
+    JOIN RecipeIngredients_STG ON RecipeIngredients_STG.RecipeID = Recipes.RecipeID
+    JOIN (
+    SELECT DISTINCT IngredientID, IngredientName FROM Ingredients
+    ) AS A ON RecipeIngredients_STG.IngredientID = A.IngredientID
+
+    GROUP BY Recipes.RecipeID
+    ) AS A
+    WHERE A.RecipeID = "{}"
+    '''.format(RecipeID)
     
+    cursor.execute(select_query)
+    return cursor.fetchall()
 
+def get_recipe_by_ingredient(IngredientList):
+    IngredientsString = ', '.join(f'"{i}"' for i in IngredientList)
+
+    select_query = '''SELECT DISTINCT RecipeID
+    FROM (
+    SELECT RecipeIngredients_STG.RecipeID, RecipeIngredients_STG.IngredientID, Ingredients.IngredientName FROM RecipeIngredients_STG
+    JOIN Ingredients ON Ingredients.IngredientID = RecipeIngredients_STG.IngredientID
+    ) AS A
+    WHERE A.IngredientName IN ({})
+    '''.format(IngredientsString)
+
+    cursor.row_factory = lambda cursor, row: row[0]
+    cursor.execute(select_query)
+    return cursor.fetchall()
 
 ## ------------- Testing Workspace ----------------
 
-db, cursor = sql_connect('RecipePublic.db')
+#db, cursor = sql_connect('RecipePublic.db')
+#print(get_recipe(db, cursor, "015284"))
+#print(get_recipe_by_ingredient(["butter","cream cheese"]))
 ##add_user(db, cursor,  "Jane", "Doe")
 ##add_ingredient(db, cursor, IngredientName = "Banana", Description = "An elongated, edible fruit, botanically a berry")
 
-recipes = parse_recipes(1)
-for recipe in recipes:
-    add_recipe(db, cursor, recipe)
+##recipes = get_recipes()
+##for recipe in recipes:
+    ##add_recipe(db, cursor, recipe)
 
 ##with open('sql_bcknd/test.sql', 'r') as sql_file:
     ##sql_script = sql_file.read()
