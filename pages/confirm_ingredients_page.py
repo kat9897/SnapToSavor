12d42821d9dcf0e0d391a84a62a6005a113dbf82
Katrina <katrinaemily.best@mail.utoronto.ca>
2024-02-05 14:43:36 -0500

    sure

--- pages/confirm_ingredients_page.py
+++ pages/confirm_ingredients_page.py
@@ -7,22 +7,24 @@ from food_api import *
 # Using Tables Ex: https://docs.taipy.io/en/release-3.0/knowledge_base/tips/using_tables/
 # Tables class: https://docs.taipy.io/en/latest/manuals/gui/viselements/table/
 
-# Bindings
-# here = os.path.dirname(os.path.abspath(__file__))
-# image = os.path.join(here, "./img/image-missing.svg") # default image without any user input
-
 def confirmed_ingred(state):
     # print(state.selected_files)
     # print(state.num_ingred)
     # print(state.ingredients_table)
     navigate(state, "recipes")
 
-def delete_ingred(state):
+def cancel_ingredients(state):
     state.selected_files = None
     state.num_ingred = 0
     state.ingredients_table = []
     state.ingredients = []
     navigate(state, to="ingredients")
+
+def food_df_on_delete(state, var_name, payload):
+    index = payload["index"] # row index
+
+    state.ingredients_table = state.ingredients_table.drop(index=index)
+    notify(state, "E", f"Deleted row at index '{index}'")
 
 def food_df_on_edit(state, var_name, payload):
     index = payload["index"] # row index
@@ -38,28 +40,19 @@ def food_df_on_edit(state, var_name, payload):
     state.ingredients_table = new_food_df
     notify(state, "I", f"Edited value from '{old_value}' to '{value}'. (index '{index}', column '{col}')")
 
+def food_df_on_add(state, var_name, payload):
+    empty_row = pd.DataFrame([[None for _ in state.ingredients_table.columns]], columns=state.ingredients_table.columns)
+    state.ingredients_table = pd.concat([empty_row, state.ingredients_table], axis=0, ignore_index=True)
+
+    notify(state, "S", f"Added a new row.")
+
 with tgb.Page() as confirm_ingredients_page:
     with tgb.layout("2 3"):
         with tgb.part(class_name="card card-bg"):
-            tgb.image(content="{selected_files}", class_name="center")
+            tgb.image(content="{selected_files}", class_name="center inline")
         with tgb.part(class_name="card card-bg"):
             tgb.text(value="Confirmed Ingredient(s): {num_ingred}")
-            tgb.table(data="{ingredients_table}", on_edit=food_df_on_edit)
+            tgb.table(data="{ingredients_table}", on_edit=food_df_on_edit, on_delete=food_df_on_delete, on_add=food_df_on_add)
         tgb.button("Confirm", class_name="success", on_action=confirmed_ingred)
-        tgb.button("Cancel", class_name="error", on_action=delete_ingred)
+        tgb.button("Cancel", class_name="error", on_action=cancel_ingredients)
 
-# confirm_ingredients_page="""
-# <|layout|columns=2 3|
-# <|card card-bg|
-# <center>
-# <|{selected_files}|image|>
-# </center>
-# |>
-# <|card card-bg|
-# <|Confirmed {num_ingred} ingredient(s):|>\n
-# <|{ingredients}|>
-# |>
-# <|Confirm|button|class_name=success|on_action=confirmed_ingred|>
-# <|Delete|button|class_name=error|on_action=delete_ingred|>
-# |>
-# """
